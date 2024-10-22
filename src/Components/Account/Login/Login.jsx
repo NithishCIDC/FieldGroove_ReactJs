5b0dcd0b137da8ba4f30ef35f57ada159ef564a2
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-10-22 17:00:05 +0530

    break

--- src/Components/Account/Login/Login.jsx
+++ src/Components/Account/Login/Login.jsx
@@ -1,42 +1,24 @@
 import React from "react";
 import logo from "../../../assets/images/logo-fieldgroove-blue.svg";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { LoginConstant } from "../../../Constants/ApplicationConstants";
 
 const Login = () => {
   return (
     <div>
-      <Container>
+      <Container className="d-flex justify-content-between px-5 mt-4">
         <div>
-          <img src={logo} alt="" width={200} />
+          <img src={logo} alt="" width={250} />
         </div>
         <div>
-          <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">
-                      Action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">
-                      Separated link
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+          <ul className="d-flex list-unstyled gap-5 text-uppercase">
+            <li>{LoginConstant.navItem1}</li>
+            <li>{LoginConstant.navItem2}</li>
+            <li>{LoginConstant.navItem3}</li>
+            <li>{LoginConstant.navItem4}</li>
+            <li>{LoginConstant.navItem5}</li>
+            <li>{LoginConstant.navItem6}</li>
+          </ul>
         </div>
       </Container>
     </div>
