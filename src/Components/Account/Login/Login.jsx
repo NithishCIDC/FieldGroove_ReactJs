5d2795b821448b5102504fa0e0a83bbed172b9d7
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-10-22 16:07:22 +0530

    added Account

---
+++ src/Components/Account/Login/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Constants/ApplicationConstants.js
@@ -0,0 +1,10 @@
+
+export const LoginConstant=
+{
+    navItem1 :"Features",
+    navItem2 :"Industries",
+    navItem3 : "FAQS",
+    navItem4 : "Contact us",
+    navItem5 : "Sign up",
+    navItem6 : "Login"
+}
