cad72e701a883df0223a707e79de55f0492a81ff
Hariprakash3102 <hariprakash.srinivasan@concertidc.com>
2024-10-23 10:24:26 +0530

    Added the style

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Login from "./Components/Account/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Register from "./Components/Account/Register/Register";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import CreateLead from "./Components/CreateLead/CreateLead";
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/CreateLead" element={<CreateLead  />} />
       </Routes>
     </BrowserRouter>
   );
