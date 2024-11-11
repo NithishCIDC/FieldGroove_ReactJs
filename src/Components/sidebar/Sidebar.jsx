6627abea8406b90f659eaac4871c1032d267b0d4
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-11-11 16:06:01 +0530

    .

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
     const NavigateLogin = () => {
         Swal.fire({
             title: "Are you sure?",
-            text: "You Want to Logout",
+            text: "You want to Logout",
             icon: "warning",
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
