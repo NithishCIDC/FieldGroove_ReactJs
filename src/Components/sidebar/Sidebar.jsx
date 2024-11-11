30352e13ba26f970c7fd5a14b3f9f1c2a2d54cce
NithishCIDC <nithish.sakthivelmurugan@concertidc.com>
2024-11-11 16:05:41 +0530

    Added Logout button

--- src/Components/sidebar/Sidebar.jsx
+++ src/Components/sidebar/Sidebar.jsx
@@ -1,9 +1,29 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import "../../CSS/Sidebar.css"
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+    const NavigateLogin = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You Want to Logout",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Logout!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                sessionStorage.removeItem("token");
+                navigate('/');
+            }
+        });
+
+    }
     return (
-        <div className="m-0 p-0 sticky-top" style={{top: "70px"}}>
+        <div className="m-0 p-0 sticky-top" style={{ top: "70px" }}>
             <div className="d-flex flex-column align-items-center pt-4">
                 <Icon icon="pajamas:task-done" style={{ color: "gray" }} width={"40"} height={"40"} />
                 <span className="side-bar-font my-1">Tasks</span>
@@ -31,6 +51,10 @@ const Sidebar = () => {
             <div className="d-flex flex-column align-items-center mt-4">
                 <Icon icon="bi:speedometer2" style={{ color: "gray" }} width={"40"} height={"40"} />
                 <span className="side-bar-font my-1">Super Admin</span>
+            </div>
+            <div className="d-flex flex-column align-items-center mt-4">
+                <Icon onClick={NavigateLogin} icon="tabler:logout-2" style={{ color: "gray" }} width={"40"} height={"40"} />
+                <span className="side-bar-font my-1">Logout</span>
             </div>
         </div>
     );
